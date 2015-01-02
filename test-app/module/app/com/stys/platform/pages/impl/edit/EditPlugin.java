1a4aa2b9b167415b958dbfbebcbe9d2dbec308b1
Alexey Stysin <alexey.stysin@gmail.com>
2015-01-02 16:39:38 +0300

    cleanup

--- test-app/module/app/com/stys/platform/pages/impl/edit/EditPlugin.java
+++
@@ -1,7 +0,0 @@
-package com.stys.platform.pages.impl.edit;
-
-import com.stys.platform.pages.Plugin;
-import com.stys.platform.pages.impl.Page;
-import com.stys.platform.pages.impl.utils.ContentResult;
-
-public abstract class EditPlugin extends Plugin<ContentResult, Page> {}
